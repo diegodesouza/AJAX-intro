9c5bb1c05b13bb53569359090ace436be1dbd8dd
Diego de Souza <diegotrus7@gmail.com>
2015-04-28 18:07:15 -0400

    Ajax call on click

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,8 +1,11 @@
-// Ajax call as soon as the page is loaded.
-$.ajax({
-  url: '/time.json',
-  dataType: 'json',
-  success: function(data){
-    alert("the time is: " + data.time);
-  }
-})
+// Ajax call on click
+$("#get-time").on('click', function(e){
+  e.preventDefault();
+  $.ajax({
+    url: '/time.json',
+    dataType: 'json',
+    success: function(data){
+      alert("the time is: " + data.time);
+    }
+  });
+});
