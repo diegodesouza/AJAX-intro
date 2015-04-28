568adcb001573375a1bea2eefc348094f68cc1ab
Diego de Souza <diegotrus7@gmail.com>
2015-04-28 18:07:59 -0400

    Slow Ajax call on click

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,11 +1,15 @@
-// Ajax call on click
+// Slow Ajax call on click
 $("#get-time").on('click', function(e){
   e.preventDefault();
+  console.log("issuing HTTP request")
   $.ajax({
-    url: '/time.json',
+    url: '/slow_time.json',
     dataType: 'json',
     success: function(data){
+      console.log("successful HTTP response received")
       alert("the time is: " + data.time);
     }
   });
+
+  console.log("I can do other things in JavaScript while we wait");
 });
