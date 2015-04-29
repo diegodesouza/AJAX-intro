3b83732742b30c5b9c74ac3c40d34c67a2a5fb39
Diego de Souza <diegotrus7@gmail.com>
2015-04-28 22:19:07 -0400

    create table

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,9 +1,27 @@
-// Using data from JSON
+//builds a table cell for a given value
+function buildCell(value){
+  return $('<td>').html(value);
+}
+
+//builds a table row for a given team
+function buildTeamRow(team){
+  var $row = $('<tr>');
+  $row.append(buildCell(team.rank));
+  $row.append(buildCell(team.name));
+  $row.append(buildCell(team.wins));
+  $row.append(buildCell(team.losses));
+  return $row
+}
+
 function loadStandings(){
   $.ajax({
     url: '/teams.json',
     success: function(data){
-      console.log(data);
+      //clear the contents of the table body
+      $('table tbody').html('');
+      for(var i = 0; i < data.length; i++){
+        $('table tbody').append(buildTeamRow(data[i]));
+      }
     },
     error: function(xhr, httpStatus, errorThrown){
       alert("Something went wrong. Please try again later.");
